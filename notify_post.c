9d3b56431de85ecb7c3ea333d6bb3db4350239c8
theioztream <daniel.calibeta@gmail.com>
2012-08-30 21:15:09 -0300

    [notify_post] Initial commit.

---
+++ notify_post.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <notify.h>
+
+int main(int argc, char **argv) {
+	if (argc <= 1) {
+		fprintf(stderr, "Usage: notify_post [notification]\n");
+		return 1;
+	}
+	
+	size_t len = 0;
+	
+	int i;
+	for (i=1; i<argc; i++)
+		len += strlen(argv[i]);
+	
+	char *not = (char *)malloc(len+(argc-1)*sizeof(char));
+	if (not == NULL) {
+		fprintf(stderr, "Memory error.\n");
+		return 2;
+	}
+	not[0] = '\0';
+	
+	for (i=1; i<argc; i++) {
+		strcat(not, argv[i]);
+		if (i+1 < argc) strcat(not, " ");
+	}
+	
+	notify_post(not);
+	
+	free(not);
+	return 0;
+}
