002a7e4cd56759393156c4efffcb8260c02f7506
theioztream <daniel.calibeta@gmail.com>
2012-12-01 19:38:20 -0200

    Does not delete the repos

--- theosinstall.sh
+++ theosinstall.sh
@@ -2,23 +2,15 @@
 
 ##%%%%%
 #% theosinstall.sh
-#% Extracted (not modified whatsoever) by theiostream on 30/08/2012
+#% Modified by theiostream on 30/08/2012
 #% Created by BigBoss, taken from 'bigbosshackertools'
 #%
 #% theosinstall -- Install theos. Nothing else.
 #% theiostream utilities
 ##%%%%%
 
-##%%
-# It would be a nice suggestion to have it clone the theos git repo instead of installing
-# this package, which might have gotten outdated.
-##%%
-
 echo "deb http://coredev.nl/cydia iphone main" > /etc/apt/sources.list.d/coredev1.nl.list
 echo "deb http://nix.howett.net/theos ./" > /etc/apt/sources.list.d/howett1.net.list
 
 apt-get update
 apt-get -y --force-yes install perl net.howett.theos
-
-rm -f /etc/apt/sources.list.d/howett1.net.list
-rm -f /etc/apt/sources.list.d/coredev1.nl.list
